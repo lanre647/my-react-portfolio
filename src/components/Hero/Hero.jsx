4f7630769fd943c9b759c2758d01ac44aa98a60d
Olanrewaju_Wiiliams <owolabilanre2006@gmail.com>
2025-02-26 10:51:52 +0100

    added the functionality on the navigation bar so that when we click it will scroll to it's respective section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />{" "}
-      <h1><span>I'm Lanre Williams,</span> full-stack developer based in Nigeria.</h1>
+      <h1>
+        <span>I'm Lanre Williams,</span> full-stack developer based in Nigeria.
+      </h1>
       <p>
         I'm a Full-stack developer from Lagos, Nigeria with 4 years experience
         in multiple companies like Microsoft, Tesla and Apple.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
